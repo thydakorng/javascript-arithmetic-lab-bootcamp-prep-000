98ea6c325cb9c6ef5cb16817258ce463fd324605
Thyda Korng <thydakorng@gmail.com>
2021-03-11 16:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,16 @@ function add5() {
 function divideBy3() {
   number /= 3
 }
+
 divideBy3()
 console.log(number)
 add5()
 console.log(number)
+
 number = 10
 add5()
 console.log(number)
-divideBy3
+divideBy3 ()
 console.log(number)
 
 function increment(n) {
