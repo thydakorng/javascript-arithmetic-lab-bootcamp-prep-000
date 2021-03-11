ad9a7014d228b9f3989ccb323d871cb4d39951e2
Thyda Korng <thydakorng@gmail.com>
2021-03-11 16:23:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function multiply(string) {
 function divide(string) {
   return (5.0/2.5)}
 
+var number = 10
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+divideBy3()
+console.log(number)
+add5()
+console.log(number)
+number = 10
+add5()
+console.log(number)
+divideBy3
+console.log(number)
+
+
